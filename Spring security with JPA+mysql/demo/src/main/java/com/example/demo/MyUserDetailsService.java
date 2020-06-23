a6ba84dc6dfd58849cbf9d7f567c17952f528c9f
sayhellotoraji <51025595+sayhellotoraji@users.noreply.github.com>
2020-06-23 19:18:14 +0530

    Initial commit
    Hard coded spring security authenticating using UserDetailsService

---
+++ Spring security with JPA+mysql/demo/src/main/java/com/example/demo/MyUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return new MyUserDetails(username);
+	}
+ 
+}
