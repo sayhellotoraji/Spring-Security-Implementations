9be8d3b101ab32451dabe48d87c828901aa16191
sayhellotoraji <51025595+sayhellotoraji@users.noreply.github.com>
2020-06-24 00:03:52 +0530

    Controller updated

--- Spring security with JPA+mysql/demo/src/main/java/com/example/demo/Controller.java
+++ Spring security with JPA+mysql/demo/src/main/java/com/example/demo/Controller.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeResource {
+public class Controller {
 	
 	@GetMapping("/")
     public String home() {
