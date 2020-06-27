8a571192e9ec864ef57d4a50ab91f2be80e043a4
sayhellotoraji <51025595+sayhellotoraji@users.noreply.github.com>
2020-06-27 18:52:05 +0530

    Updated

--- Spring security with JWT/jwt/src/main/java/com/rajasekar/jwt/Controller.java
+++ Spring security with JWT/jwt/src/main/java/com/rajasekar/jwt/Controller.java
@@ -1,17 +1,74 @@
 package com.rajasekar.jwt;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rajasekar.jwt.models.AuthenticationRequest;
+import com.rajasekar.jwt.models.AuthenticationResponse;
+import com.rajasekar.jwt.services.MyUserDetailsService;
+import com.rajasekar.jwt.util.JwtUtil;
 
 
 
 @RestController
 public class Controller {
+	
+	//Code order 2
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	//Code order 4
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+	
+	
+	//Code order 7
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+	
+	
 
 	@GetMapping("/hello")
 	public String home() {
 		return "Hello World";
+	}
+	
+	
+	//Since the every url needs to be authenticated we need to create an exception for this end point alone in SecurityConfiguration class.
+	@PostMapping("/authenticate")
+	
+
+	
+	// code order 1
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+			);
+		}
+		catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+
+		//Code order 3
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		//Code order 5
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+		
+		//Code order 6
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
 	}
 	
 }
--- Spring security with JWT/jwt/src/main/java/com/rajasekar/jwt/SecurityConfiguration.java
+++ Spring security with JWT/jwt/src/main/java/com/rajasekar/jwt/SecurityConfiguration.java
@@ -2,12 +2,17 @@ package com.rajasekar.jwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.rajasekar.jwt.filters.JwtRequestFilter;
 import com.rajasekar.jwt.services.MyUserDetailsService;
 
 @EnableWebSecurity
@@ -15,12 +20,38 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Autowired
 	private MyUserDetailsService myUserDetailsService;
+	
+	
+	//Remove session management and adding filter
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
 
+	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
 		
 		auth.userDetailsService(myUserDetailsService);
+	}
+	
+	//Code that needs to be added for the endpoint
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable()
+				.authorizeRequests().antMatchers("/authenticate").permitAll().
+						anyRequest().authenticated().and().
+						exceptionHandling().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	
+	//Create a bean of AuthenticationManager
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 	
 	
